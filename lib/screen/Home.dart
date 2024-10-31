57b8efd1204d5044eb8aab01b9cf94f3bc930a93
touil-mohamed <touil.mohamed1@hotmail.com>
2024-10-31 12:29:26 +0100

    feat: :art: edit HomePage and Font/family

--- lib/screen/Home.dart
+++ lib/screen/Home.dart
@@ -1,5 +1,6 @@
 import 'package:brico_voisin/theme/colors.dart';
 import 'package:brico_voisin/widget/delayed_animation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class MyHome extends StatelessWidget {
@@ -10,114 +11,121 @@ class MyHome extends StatelessWidget {
     return Scaffold(
         backgroundColor: const Color(0xFFFFB020),
         body: Stack(
-      children: [
-        SingleChildScrollView(
-            child: Container(
-          margin: const EdgeInsets.symmetric(
-            vertical: 60,
-            horizontal: 30,
-          ),
-          child: Column(children: [
-            const SizedBox(
-              height: 200,
-            ),
-            Delayanimation(
-              delay: 1500,
-              child: Image.asset("assets/images/Bricovoisins.png"),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            Delayanimation(
-              delay: 1000,
-              child: Container(
-                margin: const EdgeInsets.only(
-                  top: 10,
-                  bottom: 10,
-                ),
-                child: const Text(
-                  "La seule app qui recréer du ien convivial",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    color: Colors.black,
-                    fontSize: 17,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Delayanimation(
-                delay: 4500,
+          children: [
+            SingleChildScrollView(
                 child: SizedBox(
-                  //height: 70,
-                  //width: 200,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: MainColor.primaryColor,
-                      shape: const StadiumBorder(),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 35,
-                        vertical: 15,
+              child: Column(children: [
+                const SizedBox(
+                  height: 90,
+                ),
+                Delayanimation(
+                  delay: 1500,
+                  child:
+                      Image.asset("assets/images/Bricovoisins.png", width: 262),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Delayanimation(
+                  delay: 1500,
+                  child: Image.asset("assets/images/logindecoration.png",
+                      width: 575),
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Delayanimation(
+                  delay: 1000,
+                  child: Container(
+                    margin: const EdgeInsets.only(
+                      top: 10,
+                      bottom: 10,
+                    ),
+                    child: const Text(
+                      "PARTAGEZ, LOUEZ, CONSTRUISEZ L'AVENIR",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: Colors.black,
+                        fontSize: 24,
+                        fontFamily: 'AkiraExpanded',
                       ),
                     ),
-                    child: Text(
-                      'CONNEXION',
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: MainColor.secondaryColor),
-                    ),
-                    onPressed: () {
-                      // Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //       builder: (context) => const Login(),
-                      //     ));
-                    },
                   ),
-                )),
-            const SizedBox(
-              height: 20,
-            ),
-            Delayanimation(
-                delay: 4500,
-                child: SizedBox(
-                  //height: 70,
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: MainColor.primaryColor,
-                      shape: const StadiumBorder(),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 25,
-                        vertical: 15,
+                ),
+                Delayanimation(
+                    delay: 4500,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 24,
+                        horizontal: 24,
+                      ), //height: 70,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(
+                              0xFFFFD4AB), // Couleur de fond #FFD4AB
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                              side: const BorderSide(
+                                  color: Colors.black,
+                                  width: 2) // Coins arrondis
+                              ),
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 25,
+                            vertical: 15,
+                          ),
+                          shadowColor: Colors.black, // Couleur de l'ombre
+                          elevation: 10, // Hauteur de l'ombre
+                        ),
+                        child: const Text(
+                          'Inscrivez-vous',
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.black,
+                              fontFamily: 'Sora'),
+                        ),
+                        onPressed: () {
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //       builder: (context) => const Inscription(),
+                          //     ));
+                        },
                       ),
-                    ),
-                    child: Text(
-                      'INSCRIPTION',
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: MainColor.secondaryColor),
-                    ),
-                    onPressed: () {
-                      // Navigator.push(
-                      //     context,
-                      //     MaterialPageRoute(
-                      //       builder: (context) => const Inscription(),
-                      //     ));
-                    },
-                  ),
-                )),
-            const SizedBox(
-              height: 20,
-            ),
-          ]),
-        )),
-      ],
-    ));
+                    )),
+                Delayanimation(
+                    delay: 0,
+                    child: Text.rich(
+                      TextSpan(
+                        text: "Déjà inscrit ? ", // Texte normal
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontFamily: 'Sora',
+                        ),
+                        children: [
+                          TextSpan(
+                            text: "Connectez-vous", // Texte souligné et en gras
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w800, // Gras
+                                decoration: TextDecoration.underline,
+                                fontFamily: 'Sora' // Souligné
+                                ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                // Action pour le clic, actuellement vide
+                              },
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.center,
+                    ))
+              ]),
+            )),
+          ],
+        ));
   }
 }
