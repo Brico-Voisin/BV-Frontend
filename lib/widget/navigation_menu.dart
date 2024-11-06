1536b74dd47661ca987d32954cd3a2615f86bacf
Gaëtan <dapvril.gaetan@gmail.com>
2024-11-06 14:53:27 +0100

    style: :lipstick: adjust BottomNavigationBarItem appearance and interactions
    - Update bottom navigation item styles with dynamic padding and icon size - Apply conditional bold text style for selected items - Change label and icon color on selection to #FFECDA - Remove bottom border from navigation menu container - Fine-tune container padding for improved spacing and alignment - Modify background color for selected items to black with contrasting label/icon colors

--- lib/widget/navigation_menu.dart
+++ lib/widget/navigation_menu.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:brico_voisin/utilities/page_manager.dart';
 
 class NavigationMenu extends StatefulWidget {
   final List<Widget> pages;
@@ -32,51 +31,59 @@ class _NavigationMenuState extends State<NavigationMenu> {
             right: 0,
             bottom: 0,
             child: Padding(
-              padding: const EdgeInsets.only(bottom: 0, left: 20, right: 20),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: const Color(0xFFFFECDA),
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 4,
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 0,
-                      offset: const Offset(12, 12), // Ombre
+                padding: const EdgeInsets.only(bottom: 0, left: 20, right: 30),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFFFECDA),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
                     ),
-                  ],
-                ),
-                child: BottomNavigationBar(
-                  backgroundColor: Colors.transparent,
-                  selectedItemColor: Colors.black,
-                  unselectedItemColor: Colors.black,
-                  showSelectedLabels: true,
-                  showUnselectedLabels: true,
-                  currentIndex: _selectedIndex,
-                  onTap: _onItemTapped,
-                  type: BottomNavigationBarType.fixed,
-                  elevation: 0,
-                  items: [
-                    _buildBottomNavigationBarItem(
-                        'assets/images/home.svg', "Explorer", 0),
-                    _buildBottomNavigationBarItem(
-                        'assets/images/favourite.svg', "Favoris", 1),
-                    _buildBottomNavigationBarItem(
-                        'assets/images/add-rectangle.svg', "Locations", 2),
-                    _buildBottomNavigationBarItem(
-                        'assets/images/chat.svg', "Messages", 3),
-                    _buildBottomNavigationBarItem(
-                        'assets/images/profile.svg', "Profil", 4),
-                  ],
-                ),
-              ),
-            ),
+                    border: Border(
+                      top: BorderSide(
+                          color: Colors.black, width: 4), // Bordure en haut
+                      left: BorderSide(
+                          color: Colors.black, width: 4), // Bordure à gauche
+                      right: BorderSide(
+                          color: Colors.black, width: 4), // Bordure à droite
+                      // Pas de bordure inférieure
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 0,
+                        offset: const Offset(12, 12), // Ombre
+                      ),
+                    ],
+                  ),
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 15, vertical: 0),
+                    child: BottomNavigationBar(
+                      backgroundColor: Colors.transparent,
+                      selectedItemColor: Colors.black,
+                      unselectedItemColor: Colors.black,
+                      showSelectedLabels: false, // Cacher le label par défaut
+                      showUnselectedLabels: false, // Cacher le label par défaut
+                      currentIndex: _selectedIndex,
+                      onTap: _onItemTapped,
+                      type: BottomNavigationBarType.fixed,
+                      elevation: 0,
+                      items: [
+                        _buildBottomNavigationBarItem(
+                            'assets/images/home.svg', "Explorer", 0),
+                        _buildBottomNavigationBarItem(
+                            'assets/images/favourite.svg', "Favoris", 1),
+                        _buildBottomNavigationBarItem(
+                            'assets/images/add-rectangle.svg', "Locations", 2),
+                        _buildBottomNavigationBarItem(
+                            'assets/images/chat.svg', "Messages", 3),
+                        _buildBottomNavigationBarItem(
+                            'assets/images/profile.svg', "Profil", 4),
+                      ],
+                    ),
+                  ),
+                )),
           ),
         ],
       ),
@@ -85,18 +92,40 @@ class _NavigationMenuState extends State<NavigationMenu> {
 
   BottomNavigationBarItem _buildBottomNavigationBarItem(
       String assetPath, String label, int index) {
+    bool isSelected = _selectedIndex == index;
+
     return BottomNavigationBarItem(
-      label: label,
+      label: '',
       icon: Container(
-        padding: const EdgeInsets.all(8),
+        width: 80,
+        padding: const EdgeInsets.symmetric(vertical: 8),
         decoration: BoxDecoration(
-          color: _selectedIndex == index ? Colors.black : Colors.transparent,
-          borderRadius: BorderRadius.circular(10),
+          color: isSelected ? Colors.black : Colors.transparent,
+          borderRadius: BorderRadius.circular(12),
         ),
-        child: SvgPicture.asset(
-          assetPath,
-          color: _selectedIndex == index ? Colors.white : Colors.black,
-          height: 24,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SvgPicture.asset(
+              assetPath,
+              color: isSelected ? const Color(0xFFFFECDA) : Colors.black,
+              height: 24,
+            ),
+            const SizedBox(height: 6),
+            Text(
+              label,
+              style: TextStyle(
+                color: isSelected ? const Color(0xFFFFECDA) : Colors.black,
+                fontSize: 11,
+                fontWeight: isSelected
+                    ? FontWeight.bold
+                    : FontWeight.normal, // Gras quand sélectionné
+                fontFamily: 'Sora',
+              ),
+              overflow: TextOverflow.visible, // Désactive le tronquage
+              maxLines: 1,
+            ),
+          ],
         ),
       ),
     );
