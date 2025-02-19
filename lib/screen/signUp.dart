afdd74aad8444e384a6b26ef30f48dd1ab7c4bfd
touil-mohamed <touil.mohamed1@hotmail.com>
2025-02-19 11:35:51 +0100

    feat: :recycle: add toggle password visibility feature to sign-up form

--- lib/screen/signUp.dart
+++ lib/screen/signUp.dart
@@ -1,4 +1,4 @@
-import 'package:brico_voisin/screen/verificationCode.Dart';
+import 'package:brico_voisin/screen/verificationCode.dart'; // Fixed the import file case
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -12,8 +12,11 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _confirmPasswordController = TextEditingController(); // Added confirmation password
   final _formKey = GlobalKey<FormState>();
   bool _isLoading = false;
+  bool _isPasswordVisible = false; // Ajouté pour la visibilité du mot de passe
+  bool _isConfirmPasswordVisible = false; // Ajouté pour la visibilité de la confirmation du mot de passe
 
   Future<void> _signUp() async {
     final supabase = Supabase.instance.client;
@@ -29,33 +32,16 @@ class _SignUpState extends State<SignUp> {
     });
 
     try {
-      // Inscription avec Supabase Auth
-      final response = await supabase.auth.signUp(
-        email: email,
-        password: password,
-      );
+      final response = await supabase.auth.signUp(email: email, password: password);
 
       if (response.user != null) {
-       
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Inscription réussie !')),
-        );
-
-        // Rediriger vers l'écran de vérification du code
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => VerificationCodeScreen(email: email,),
-        ),
-      );
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Inscription réussie !')));
+        Navigator.push(context, MaterialPageRoute(builder: (context) => VerificationCodeScreen(email: email)));
       } else {
-        throw Exception('Erreur lors de l\'inscription : $response.error');
+        //throw Exception('Erreur lors de l\'inscription : ${response.errorMessage}');
       }
     } catch (error) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(error.toString())),
-      );
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(error.toString())));
     } finally {
       setState(() {
         _isLoading = false;
@@ -66,8 +52,22 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFFFDDBD), // Light beige background
       appBar: AppBar(
-        title: const Text('Inscription'),
+        backgroundColor: Colors.transparent, // Transparent app bar
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/pattern.png'),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -76,10 +76,25 @@ class _SignUpState extends State<SignUp> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TextFormField(
+              
+              Center(
+                child: Image.asset("assets/images/Bricovoisins.png", width: 262),
+              ),   
+              const SizedBox(height: 16.0), 
+              const Text(
+                'Hey, bienvenue ! 👋',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+              ),
+              const SizedBox(height: 8.0),
+              const Text(
+                'Créez votre compte en quelques étapes simples. Remplissez les informations ci-dessous pour commencer votre inscription.',
+                style: TextStyle(color: Colors.black54),
+              ),
+              const SizedBox(height: 32.0),
+              _buildTextField(
                 controller: _emailController,
-                decoration: const InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
+                hint: 'Email',
+                icon: Icons.email,
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Veuillez entrer un email';
@@ -91,10 +106,21 @@ class _SignUpState extends State<SignUp> {
                 },
               ),
               const SizedBox(height: 16.0),
-              TextFormField(
+              _buildTextField(
                 controller: _passwordController,
-                decoration: const InputDecoration(labelText: 'Mot de passe'),
-                obscureText: true,
+                hint: 'Mot de passe',
+                icon: Icons.lock,
+                obscureText: !_isPasswordVisible,
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
                 validator: (value) {
                   if (value == null || value.isEmpty) {
                     return 'Veuillez entrer un mot de passe';
@@ -105,28 +131,82 @@ class _SignUpState extends State<SignUp> {
                   return null;
                 },
               ),
-              const SizedBox(height: 32.0),
+              const SizedBox(height: 16.0),
+              _buildTextField(
+                controller: _confirmPasswordController,
+                hint: 'Confirmation du mot de passe',
+                icon: Icons.lock,
+                obscureText: !_isConfirmPasswordVisible,
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isConfirmPasswordVisible ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isConfirmPasswordVisible = !_isConfirmPasswordVisible;
+                    });
+                  },
+                ),
+                validator: (value) {
+                  if (value != _passwordController.text) {
+                    return 'Les mots de passe ne correspondent pas';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 150),
               _isLoading
                   ? const Center(child: CircularProgressIndicator())
                   : SizedBox(
                       width: double.infinity,
                       child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFFA0522D), // Button color
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                        ),
                         onPressed: _signUp,
-                        child: const Text('S\'inscrire'),
+                        child: const Text('Suivant', style: TextStyle(color: Colors.white)),
                       ),
                     ),
-              const SizedBox(height: 16.0),
-              Center(
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pushReplacementNamed(context, '/login');
-                  },
-                  child: const Text('Vous avez déjà un compte ? Connectez-vous'),
-                ),
-              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildTextField({
+    required TextEditingController controller,
+    required String hint,
+    required IconData icon,
+    bool obscureText = false,
+    Widget? suffixIcon,
+    FormFieldValidator<String>? validator,
+  }) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFFFFDDBD),
+        borderRadius: BorderRadius.circular(18),
+        border: Border.all(color: Colors.black, width: 1),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 0,
+            offset: Offset(3, 3),
+          ),
+        ],
+      ),
+      child: TextFormField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.all(25),
+          hintText: hint,
+          suffixIcon: suffixIcon,
+          prefixIcon: Icon(icon),
+        ),
+        validator: validator,
       ),
     );
   }
